10917af7be49e5d8be1f99449eadcdafb5b00e31
Guilherme Souza Araújo <guilhermesouzae21@gmail.com>
2021-10-20 22:20:56 -0300

    Criação do javascript e salvar dados no localStorage

---
+++ Codigo/cadastro/cadastro.js
@@ -0,0 +1,26 @@
+const botaoCadastrar = document.getElementById("btnCadastrar");
+botaoCadastrar.addEventListener("click", main);
+
+
+function main(){
+    const dadosCadastro = resgataDados();
+    salvaCadastro(dadosCadastro);
+}
+
+function resgataDados(){
+    return {
+        nomeCompleto: document.getElementById("inptNome").value,
+        email: document.getElementById("inptEmail").value,
+        senha: document.getElementById("inptSenha").value
+    }
+}
+
+function salvaCadastro(dados){
+    try{
+        localStorage.setItem("nomeCompleto", dados.nomeCompleto);
+        localStorage.setItem("email", dados.email);
+        localStorage.setItem("senha", dados.senha);
+    }catch(erro){
+        window.alert("Erro: " + erro);
+    }
+}
